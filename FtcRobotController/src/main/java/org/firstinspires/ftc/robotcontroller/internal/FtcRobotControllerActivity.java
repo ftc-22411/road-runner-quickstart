fff24b5a24e4036679d3b8e06da6c2e5030f2dc1
DeadFish2026 <Fischer.Wheeler@k12.nd.us>
2024-10-06 09:40:47 -0500

    First

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -303,7 +303,7 @@ public class FtcRobotControllerActivity extends Activity
     setContentView(R.layout.activity_ftc_controller);
 
     preferencesHelper = new PreferencesHelper(TAG, context);
-    preferencesHelper.writeBooleanPrefIfDifferent(context.getString(R.string.pref_rc_connected), true);
+    preferencesHelper.writeBooleanPrefIfDifferent(context.getString(org.opencv.R.string.pref_rc_connected), true);
     preferencesHelper.getSharedPreferences().registerOnSharedPreferenceChangeListener(sharedPreferencesListener);
 
     // Check if this RC app is from a later FTC season than what was installed previously
@@ -312,10 +312,10 @@ public class FtcRobotControllerActivity extends Activity
     if (ftcSeasonYearOfCurrentlyInstalledRc > ftcSeasonYearOfPreviouslyInstalledRc) {
       preferencesHelper.writeIntPrefIfDifferent(getString(R.string.pref_ftc_season_year_of_current_rc), ftcSeasonYearOfCurrentlyInstalledRc);
       // Since it's a new FTC season, we should reset certain settings back to their default values.
-      preferencesHelper.writeBooleanPrefIfDifferent(getString(R.string.pref_warn_about_2_4_ghz_band), true);
-      preferencesHelper.writeBooleanPrefIfDifferent(getString(R.string.pref_warn_about_obsolete_software), true);
-      preferencesHelper.writeBooleanPrefIfDifferent(getString(R.string.pref_warn_about_mismatched_app_versions), true);
-      preferencesHelper.writeBooleanPrefIfDifferent(getString(R.string.pref_warn_about_incorrect_clocks), true);
+      preferencesHelper.writeBooleanPrefIfDifferent(getString(org.opencv.R.string.pref_warn_about_2_4_ghz_band), true);
+      preferencesHelper.writeBooleanPrefIfDifferent(getString(org.opencv.R.string.pref_warn_about_obsolete_software), true);
+      preferencesHelper.writeBooleanPrefIfDifferent(getString(org.firstinspires.ftc.apriltag.R.string.pref_warn_about_mismatched_app_versions), true);
+      preferencesHelper.writeBooleanPrefIfDifferent(getString(org.opencv.R.string.pref_warn_about_incorrect_clocks), true);
     }
 
     entireScreenLayout = (LinearLayout) findViewById(R.id.entire_screen);
@@ -399,7 +399,7 @@ public class FtcRobotControllerActivity extends Activity
     RobotLog.logDeviceInfo();
     AndroidBoard.getInstance().logAndroidBoardInfo();
 
-    if (preferencesHelper.readBoolean(getString(R.string.pref_wifi_automute), false)) {
+    if (preferencesHelper.readBoolean(getString(com.google.blocks.R.string.pref_wifi_automute), false)) {
       initWifiMute(true);
     }
 
@@ -504,11 +504,11 @@ public class FtcRobotControllerActivity extends Activity
     if (Device.isRevControlHub() == true) {
       networkType = NetworkType.RCWIRELESSAP;
     } else {
-      networkType = NetworkType.fromString(preferencesHelper.readString(context.getString(R.string.pref_pairing_kind), NetworkType.globalDefaultAsString()));
+      networkType = NetworkType.fromString(preferencesHelper.readString(context.getString(org.opencv.R.string.pref_pairing_kind), NetworkType.globalDefaultAsString()));
     }
 
     // update the app_settings
-    preferencesHelper.writeStringPrefIfDifferent(context.getString(R.string.pref_pairing_kind), networkType.toString());
+    preferencesHelper.writeStringPrefIfDifferent(context.getString(org.opencv.R.string.pref_pairing_kind), networkType.toString());
   }
 
   @Override
@@ -703,7 +703,7 @@ public class FtcRobotControllerActivity extends Activity
       requestRobotSetup(LynxConstants.isRevControlHub()
         ? new Runnable() {
             @Override public void run() {
-              showRestartRobotCompleteToast(R.string.toastRobotSetupComplete);
+              showRestartRobotCompleteToast(com.google.blocks.R.string.toastRobotSetupComplete);
             }
           }
         : null);
@@ -757,7 +757,7 @@ public class FtcRobotControllerActivity extends Activity
     shutdownRobot();
     requestRobotSetup(new Runnable() {
       @Override public void run() {
-        showRestartRobotCompleteToast(R.string.toastRestartRobotComplete);
+        showRestartRobotCompleteToast(com.google.blocks.R.string.toastRestartRobotComplete);
         }
       });
   }
@@ -800,10 +800,10 @@ public class FtcRobotControllerActivity extends Activity
 
   protected class SharedPreferencesListener implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-      if (key.equals(context.getString(R.string.pref_app_theme))) {
-        ThemedActivity.restartForAppThemeChange(getTag(), getString(R.string.appThemeChangeRestartNotifyRC));
-      } else if (key.equals(context.getString(R.string.pref_wifi_automute))) {
-        if (preferencesHelper.readBoolean(context.getString(R.string.pref_wifi_automute), false)) {
+      if (key.equals(context.getString(org.opencv.R.string.pref_app_theme))) {
+        ThemedActivity.restartForAppThemeChange(getTag(), getString(org.opencv.R.string.appThemeChangeRestartNotifyRC));
+      } else if (key.equals(context.getString(com.google.blocks.R.string.pref_wifi_automute))) {
+        if (preferencesHelper.readBoolean(context.getString(com.google.blocks.R.string.pref_wifi_automute), false)) {
           initWifiMute(true);
         } else {
           initWifiMute(false);
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/PermissionValidatorWrapper.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/PermissionValidatorWrapper.java
@@ -60,19 +60,19 @@ public class PermissionValidatorWrapper extends PermissionValidatorActivity {
 
     public String mapPermissionToExplanation(final String permission) {
         if (permission.equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            return Misc.formatForUser(R.string.permRcWriteExternalStorageExplain);
+            return Misc.formatForUser(org.openftc.apriltag.R.string.permRcWriteExternalStorageExplain);
         } else if (permission.equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            return Misc.formatForUser(R.string.permRcReadExternalStorageExplain);
+            return Misc.formatForUser(org.opencv.R.string.permRcReadExternalStorageExplain);
         } else if (permission.equals(Manifest.permission.CAMERA)) {
-            return Misc.formatForUser(R.string.permRcCameraExplain);
+            return Misc.formatForUser(org.opencv.R.string.permRcCameraExplain);
         } else if (permission.equals(Manifest.permission.ACCESS_COARSE_LOCATION)) {
-            return Misc.formatForUser(R.string.permAccessLocationExplain);
+            return Misc.formatForUser(com.qualcomm.robotcore.R.string.permAccessLocationExplain);
         } else if (permission.equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            return Misc.formatForUser(R.string.permAccessLocationExplain);
+            return Misc.formatForUser(com.qualcomm.robotcore.R.string.permAccessLocationExplain);
         } else if (permission.equals(Manifest.permission.READ_PHONE_STATE)) {
-            return Misc.formatForUser(R.string.permReadPhoneState);
+            return Misc.formatForUser(org.opencv.R.string.permReadPhoneState);
         }
-        return Misc.formatForUser(R.string.permGenericExplain);
+        return Misc.formatForUser(org.opencv.R.string.permGenericExplain);
     }
 
     @Override
